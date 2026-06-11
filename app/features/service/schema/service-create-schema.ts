e617e983284cd82ba431d669d5c3c7cd81bd7bc1
G <deanfarhan46@gmail.com>
2026-06-11 12:26:44 +0700

    fix type error/warning

--- app/features/service/schema/service-create-schema.ts
+++ app/features/service/schema/service-create-schema.ts
@@ -3,9 +3,9 @@ import z from "zod";
 export const serviceCreateSchema = z.object({
     name: z.string().min(1, "Nama layanan harus diisi"),
     description: z.string().optional(),
-    price: z.coerce.number().min(1, "Harga layanan harus diisi"),
-    pricing_type: z.enum(["one_time", "hourly"], "Jenis harga tidak valid"),
-    duration: z.coerce.number().min(1, "Durasi layanan harus diisi"),
+    price: z.number().min(1, "Harga layanan harus diisi"),
+    pricing_type: z.enum(["one_time", "hourly"]),
+    duration: z.number().min(1, "Durasi layanan harus diisi"),
     is_active: z.boolean(),
 });
 
