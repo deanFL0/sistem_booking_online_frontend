68e8baff3ec3c0f55c91a6d42623534e1c0ad31c
G <deanfarhan46@gmail.com>
2026-06-17 20:49:12 +0700

    change data table pagination style

--- app/components/data-table/data-table-pagination.tsx
+++ app/components/data-table/data-table-pagination.tsx
@@ -1,5 +1,6 @@
 import type { Table } from "@tanstack/react-table"
 import { Button } from "../ui/button"
+import { ChevronFirst, ChevronLast, ChevronLeft, ChevronRight } from "lucide-react"
 
 type Props<TData> = {
     table: Table<TData>
@@ -9,53 +10,55 @@ export function DataTablePagination<TData>({
     table,
 }: Props<TData>) {
     return (
-        <div className="flex items-center justify-end space-x-2">
+        <div className="flex items-center justify-end space-x-2 p-4">
             <div className="flex-1 text-sm text-muted-foreground">
                 {table.getFilteredRowModel().rows.length} row(s) total.
             </div>
 
             <Button
                 variant="outline"
-                size="sm"
+                size="lg"
                 onClick={() => table.setPageIndex(0)}
                 disabled={
                     !table.getCanPreviousPage()
                 }
             >
-                First
+                <div className="flex items-center gap-1">
+                    <ChevronFirst /> First
+                </div>
             </Button>
 
             <Button
                 variant="outline"
-                size="sm"
+                size="lg"
                 onClick={() => table.previousPage()}
                 disabled={
                     !table.getCanPreviousPage()
                 }
             >
-                Previous
+                <ChevronLeft />Previous
             </Button>
 
             <Button
                 variant="outline"
-                size="sm"
+                size="lg"
                 onClick={() => table.nextPage()}
                 disabled={
                     !table.getCanNextPage()
                 }
             >
-                Next
+                Next <ChevronRight />
             </Button>
 
             <Button
                 variant="outline"
-                size="sm"
+                size="lg"
                 onClick={() => table.setPageIndex(table.getPageCount() - 1)}
                 disabled={
                     !table.getCanNextPage()
                 }
             >
-                Last
+                Last <ChevronLast />
             </Button>
         </div>
     )
